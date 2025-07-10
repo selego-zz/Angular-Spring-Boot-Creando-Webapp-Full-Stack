06e74985fec8ce17982087d4793f85a54ea90d1a
Bruno Marti Luis <selego@gmail.com>
2025-07-10 14:17:11 +0200

    ngFor - ngIf

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Hola Mundo angular';
+  users = ['Pepe', 'Maria', 'Juan', 'Andres'];
+  usersEmpty: string[] = [];
 }
