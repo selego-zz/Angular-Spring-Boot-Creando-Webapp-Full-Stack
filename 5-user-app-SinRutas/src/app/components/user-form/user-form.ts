7c881d302081fb58069410e94229dd0e90834afb
Bruno Marti Luis <selego@gmail.com>
2025-08-10 23:54:15 +0200

    Respaldo users Sin rutas

---
+++ 5-user-app-SinRutas/src/app/components/user-form/user-form.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'user-form',
+  imports: [FormsModule],
+  templateUrl: './user-form.html',
+})
+export class UserFormComponent {
+  @Input() user: User;
+  @Output() userEmitter: EventEmitter<User> = new EventEmitter();
+  @Output() closeEventEmitter: EventEmitter<boolean> = new EventEmitter();
+
+  constructor() {
+    this.user = new User();
+  }
+  OnClear(userForm: NgForm) {
+    this.user = new User();
+    userForm.reset();
+    userForm.resetForm();
+  }
+  OnSubmit(userForm: NgForm): void {
+    if (userForm.valid) {
+      console.log(this.user);
+      this.userEmitter.emit(this.user);
+    }
+    this.user = new User();
+    userForm.reset();
+    userForm.resetForm();
+  }
+
+  onClose() {
+    this.closeEventEmitter.emit();
+  }
+}
