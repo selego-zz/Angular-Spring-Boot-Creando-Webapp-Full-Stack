7fb49c9e15455b0f7387761c6ccf16932a5bcd66
Bruno Marti Luis <selego@gmail.com>
2025-08-20 14:23:09 +0200

    El front pregunta al back findAll

--- 5-user-app/src/app/services/userService.ts
+++ 5-user-app/src/app/services/userService.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { Observable, of } from 'rxjs';
-import { MockUsers } from '../Repository/MockUsers';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private readonly users: User[] = MockUsers;
+  private readonly users: User[] = [];
 
-  constructor() {}
+  constructor(private readonly http: HttpClient) {}
 
   findAll(): Observable<User[]> {
-    return of(this.users);
+    return this.http.get<User[]>('http://localhost:8080/api/users');
   }
 }
--- b1-usersApp/src/main/java/com/users/usersApp/controllers/UserController.java
+++ b1-usersApp/src/main/java/com/users/usersApp/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.users.usersApp.entities.User;
 import com.users.usersApp.services.UserService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
