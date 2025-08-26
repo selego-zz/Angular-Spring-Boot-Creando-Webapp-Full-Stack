46ba095fa6e233286f8a3446bdf7c37829db980c
Bruno Marti Luis <selego@gmail.com>
2025-08-26 09:35:11 +0200

    Back - implementada validacion

--- b1-usersApp/src/main/java/com/users/usersApp/entities/User.java
+++ b1-usersApp/src/main/java/com/users/usersApp/entities/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.*;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -16,9 +19,20 @@ public class User {
 	@GeneratedValue(strategy = IDENTITY)
 	private Long id;
 	
+	@NotBlank
 	private String name;
+
+	@NotBlank
 	private String lastname;
+
+	@NotBlank
+	@Email
 	private String email;
+
+	@NotBlank
+	@Size(min = 4, max = 12)
 	private String username;
+
+	@NotBlank
 	private String password;
 }
